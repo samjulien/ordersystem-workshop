bcf4c245275e091f8367fb3ae960af9b26bf20b0
Sam Julien <spjulien@gmail.com>
2018-07-13 13:02:30 -0700

    First steps into TypeScript

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,6 +1,18 @@
 module.exports = {
-  entry: './app.js',
+  entry: './app.ts',
   output: {
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
   }
 };
